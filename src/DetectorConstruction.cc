8c922742a8ba963d799e2711b2384d940399f7f9
J-Penas <62746940+J-Penas@users.noreply.github.com>
2023-10-02 12:16:17 +0200

    Initial upload

---
+++ src/DetectorConstruction.cc
@@ -0,0 +1,265 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+/// \file DetectorConstruction.cc
+/// \brief Implementation of the DetectorConstruction class
+//
+//
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+#include "DetectorConstruction.hh"
+#include "DetectorMessenger.hh"
+#include "G4Material.hh"
+#include "G4NistManager.hh"
+
+#include "G4Box.hh"
+#include "G4LogicalVolume.hh"
+#include "G4PVPlacement.hh"
+
+#include "G4GeometryManager.hh"
+#include "G4PhysicalVolumeStore.hh"
+#include "G4LogicalVolumeStore.hh"
+#include "G4SolidStore.hh"
+#include "G4RunManager.hh"
+
+#include "G4UnitsTable.hh"
+#include "G4SystemOfUnits.hh"
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+DetectorConstruction::DetectorConstruction()
+:G4VUserDetectorConstruction(),
+ fPBox(0), fLBox(0), fMaterial(0), fDetectorMessenger(0)
+{
+  fBoxSize = 1*m;
+  DefineMaterials();
+  SetMaterial("Galactic");  
+  fDetectorMessenger = new DetectorMessenger(this);
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+DetectorConstruction::~DetectorConstruction()
+{ delete fDetectorMessenger;}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+G4VPhysicalVolume* DetectorConstruction::Construct()
+{
+  return ConstructVolumes();
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void DetectorConstruction::DefineMaterials()
+{
+  // specific element name for thermal neutronHP
+  // (see G4ParticleHPThermalScatteringNames.cc)
+
+  G4int ncomponents, natoms;
+  G4double massfraction;
+
+  G4double Vdens = 1.e-25*g/cm3;
+  G4double Vpres = 1.e-19*pascal;
+  G4double Vtemp = 0.1*kelvin;
+  
+  G4double a, z;
+
+  // vacuum
+  G4Material* Vacc = new G4Material("Galactic", z=1, a=1.01*g/mole, Vdens, kStateGas, Vtemp, Vpres);
+
+  // pressurized water
+  G4Element* H  = new G4Element("TS_H_of_Water" ,"H" , 1., 1.0079*g/mole);
+  G4Element* O  = new G4Element("Oxygen"        ,"O" , 8., 16.00*g/mole);
+  G4Material* H2O = 
+  new G4Material("Water_ts", 1.000*g/cm3, ncomponents=2,
+                         kStateLiquid, 593*kelvin, 150*bar);
+  H2O->AddElement(H, natoms=2);
+  H2O->AddElement(O, natoms=1);
+  H2O->GetIonisation()->SetMeanExcitationEnergy(78.0*eV);
+  
+  // heavy water
+  G4Isotope* H2 = new G4Isotope("H2",1,2);
+  G4Element* D  = new G4Element("TS_D_of_Heavy_Water", "D", 1);
+  D->AddIsotope(H2, 100*perCent);  
+  G4Material* D2O = new G4Material("HeavyWater", 1.11*g/cm3, ncomponents=2,
+                        kStateLiquid, 293.15*kelvin, 1*atmosphere);
+  D2O->AddElement(D, natoms=2);
+  D2O->AddElement(O, natoms=1);
+  
+  // graphite
+  G4Isotope* C12 = new G4Isotope("C12", 6, 12);  
+  G4Element* C   = new G4Element("TS_C_of_Graphite","C", ncomponents=1);
+  C->AddIsotope(C12, 100.*perCent);
+  G4Material* graphite = 
+  new G4Material("graphite", 2.27*g/cm3, ncomponents=1,
+                         kStateSolid, 293*kelvin, 1*atmosphere);
+  graphite->AddElement(C, natoms=1);
+
+  // air
+  G4Element* N = new G4Element("Nitrogen", "N", 7., 14.01*g/mole);
+  G4Material* Air = new G4Material("air", 1.290*mg/cm3, ncomponents=2, kStateGas, 293*kelvin, 1*atmosphere);
+  Air->AddElement(N, massfraction=70.*perCent);
+  Air->AddElement(O, massfraction=30.*perCent);
+
+  // iron
+  G4Isotope* Fe56 = new G4Isotope("Fe56", 26, 56);
+  G4Element* Fe = new G4Element("TS_Iron_Metal", "Fe", ncomponents=1);
+  Fe->AddIsotope(Fe56, 100.*perCent);
+  G4Material* iron = new G4Material("iron", 7.874*g/cm3, ncomponents=1, kStateSolid, 293*kelvin, 1*atmosphere);
+  iron->AddElement(Fe, natoms=1);
+
+  // polyethilene
+  G4Element* Hpe = new G4Element("TS_H_of_Polyethylene", "H", 1, 1.0079*g/mole);
+  G4Element* Cpe = new G4Element("Carbon", "C", 6, 12.01*g/mole);
+  G4Material* polyethylene = new G4Material("polyethylene", 0.93*g/cm3, ncomponents=2, kStateSolid, 293*kelvin, 1*atmosphere);
+  polyethylene->AddElement(Hpe, natoms=4);
+  polyethylene->AddElement(Cpe, natoms=2);
+  
+ ///G4cout << *(G4Material::GetMaterialTable()) << G4endl;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+G4Material* DetectorConstruction::MaterialWithSingleIsotope( G4String name,
+                           G4String symbol, G4double density, G4int Z, G4int A)
+{
+ // define a material from an isotope
+ //
+ G4int ncomponents;
+ G4double abundance, massfraction;
+
+ G4Isotope* isotope = new G4Isotope(symbol, Z, A);
+ 
+ G4Element* element  = new G4Element(name, symbol, ncomponents=1);
+ element->AddIsotope(isotope, abundance= 100.*perCent);
+ 
+ G4Material* material = new G4Material(name, density, ncomponents=1);
+ material->AddElement(element, massfraction=100.*perCent);
+
+ return material;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+G4VPhysicalVolume* DetectorConstruction::ConstructVolumes()
+{
+  // Cleanup old geometry
+  G4GeometryManager::GetInstance()->OpenGeometry();
+  G4PhysicalVolumeStore::GetInstance()->Clean();
+  G4LogicalVolumeStore::GetInstance()->Clean();
+  G4SolidStore::GetInstance()->Clean();
+
+  // Get materials
+  auto iron = G4Material::GetMaterial("iron");
+  auto polyethylene = G4Material::GetMaterial("polyethylene");
+  auto D2O = G4Material::GetMaterial("HeavyWater");
+  auto Vacc = G4Material::GetMaterial("Galactic");
+
+  // world
+  G4Box*
+  sBox = new G4Box("Container",                         //its name
+                   fBoxSize/2,fBoxSize/2,fBoxSize/2);   //its dimensions
+
+  fLBox = new G4LogicalVolume(sBox,                     //its shape
+                             fMaterial,                 //its material
+                             fMaterial->GetName());     //its name
+
+  fPBox = new G4PVPlacement(0,                          //no rotation
+                            G4ThreeVector(),            //at (0,0,0)
+                            fLBox,                      //its logical volume
+                            fMaterial->GetName(),                    //its name
+                            0,                          //its mother  volume
+                            false,                      //no boolean operation
+                            0);                         //copy number
+
+  // shielding
+  G4double ShThick = 15*cm;
+  G4double ShSize = 100*cm;
+  G4double ShPos = 7.5*cm;
+
+  G4Box* sShield = new G4Box("shield",
+                    ShThick/2,ShSize/2,ShSize/2);
+
+  auto fLShield = new G4LogicalVolume(sShield,
+                                      polyethylene,
+                                      "Shield");
+
+  auto fPShield = new G4PVPlacement(0,
+                                    G4ThreeVector(ShPos,0.*cm,0.*cm),
+                                    fLShield,
+                                    "Shield",
+                                    fLBox,
+                                    false,
+                                    0);
+
+  PrintParameters();
+  
+  //always return the root volume
+  //
+  return fPBox;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void DetectorConstruction::PrintParameters()
+{
+  G4cout << "\n The Box is " << G4BestUnit(fBoxSize,"Length")
+         << " of " << fMaterial->GetName() 
+         << "\n \n" << fMaterial << G4endl;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void DetectorConstruction::SetMaterial(G4String materialChoice)
+{
+  // search the material by its name
+  G4Material* pttoMaterial =
+     G4NistManager::Instance()->FindOrBuildMaterial(materialChoice);   
+  
+  if (pttoMaterial) { 
+    if(fMaterial != pttoMaterial) {
+      fMaterial = pttoMaterial;
+      if(fLBox) { fLBox->SetMaterial(pttoMaterial); }
+      G4RunManager::GetRunManager()->PhysicsHasBeenModified();
+    }
+  } else {
+    G4cout << "\n--> warning from DetectorConstruction::SetMaterial : "
+           << materialChoice << " not found" << G4endl;
+  }              
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void DetectorConstruction::SetSize(G4double value)
+{
+  fBoxSize = value;
+  G4RunManager::GetRunManager()->ReinitializeGeometry();
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
